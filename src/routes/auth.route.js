bae364057e6122b0d3447667dd570b0842e619b5
Suraj Vijay <surajcpp115@gmail.com>
2022-03-06 23:12:44 +0530

    updated cql queries and auth route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -27,7 +27,7 @@ router.post("/login", async (req, res) => {
           return res.status(400).json("Already logged in");
 
         const sessionToken = await generateAccessToken(user.id);
-        return res.status(200).json({id: user.id, token: sessionToken, username});
+        return res.status(200).json({id: user.id, token: sessionToken});
     } catch (err) {
         return res.status(500).json(err);
     }
