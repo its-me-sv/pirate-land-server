1a593fe9607b5947a44f902f0356ee3c5c06b19b
Suraj Vijay <surajcpp115@gmail.com>
2022-03-06 11:07:13 +0530

    implemented creating new user

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,9 +2,31 @@
 const router = require('express').Router();
 const bcrypt = require("bcrypt");
 
+// custom
+const client = require("../utils/astra-database.util");
+
 // create new user
 router.post("/create", async (req, res) => {
-    return res.status(200).json("Create user with this route");
+    const {name, username, password} = req.body;
+    try {
+        const salt = await bcrypt.genSalt(+process.env.SALT);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        
+        const QUERY = `
+          INSERT INTO users (id, name, username, password_hash, created_at)
+          VALUES (uuid(), ?, ?, ?, now());
+        `;
+        const VALUES = [name, username, hashedPassword];
+
+        const result = await client.execute(QUERY, VALUES);
+        console.log(result);
+
+        return res.status(200).json("Account created successfully");
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json("Error while creating account");
+    }
 });
 
 module.exports = router;
