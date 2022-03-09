ce2d713e38371a4c4a9b45c7f934210b8977e588
Suraj Vijay <surajcpp115@gmail.com>
2022-03-09 20:31:03 +0530

    route for scoreboards table

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@
 const validationRoute = require('./validation.route');
 const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
+const scoreboard = require('./scoreboard.route');
 
 // combining all custom routes
 const combineRoutes = app => {
     app.use("/api/validation", validationRoute);
     app.use("/api/auth", authRoute);
     app.use("/api/users", userRoute);
+    app.use("/api/scoreboard", scoreboard);
 };
 
 module.exports = combineRoutes;
