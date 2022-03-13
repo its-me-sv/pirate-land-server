5b6aa5e968f49de50fff2583e3e71c0eef88a7da
Suraj Vijay <surajcpp115@gmail.com>
2022-03-14 05:14:09 +0530

    updating the team boards on click

--- src/routes/boards.route.js
+++ src/routes/boards.route.js
@@ -45,4 +45,42 @@ router.put(`/update_board`, async (req, res) => {
     }
 });
 
+router.put(`/make_move`, async (req, res) => {
+    try {
+        const {currTeamId, typ, idx, oppTeamId} = req.body;
+        // fetching team board and converting it
+        let QUERY = `SELECT board FROM boards WHERE id = ?;`;
+        let VALUE = [currTeamId];
+        let myTeamBoard = (await client.execute(QUERY, VALUE)).rows[0].board;
+        myTeamBoard = myTeamBoard.split(',').map(v => Number(v));
+        // fetching opp board and converting it
+        VALUE = [oppTeamId];
+        let oppTeamBoard = (await client.execute(QUERY, VALUE)).rows[0].board;
+        oppTeamBoard = oppTeamBoard.split(',').map(v => Number(v));
+        // opponent missed
+        if (!(oppTeamBoard[idx] != 0)) return res.status(200).json("miss");
+        // opponent hit
+        // find opp typ
+        let oppTyp = oppTeamBoard[idx];
+        // update both boards
+        myTeamBoard[idx] = -oppTyp;
+        oppTeamBoard[idx] = -oppTyp;
+        // convert both boards
+        myTeamBoard = myTeamBoard.join();
+        oppTeamBoard = oppTeamBoard.join();
+        // update the teams boards
+        // updating my team board
+        QUERY = `UPDATE boards SET board = ? WHERE id = ?;`;
+        VALUE = [myTeamBoard, currTeamId];
+        await client.execute(QUERY, VALUE);
+        // updating opp team board
+        VALUE = [oppTeamBoard, oppTeamId];
+        await client.execute(QUERY, VALUE);
+        return res.status(200).json("hit");
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+});
+
 module.exports = router;
